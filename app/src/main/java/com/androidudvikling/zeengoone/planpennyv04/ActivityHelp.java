8073fae740b757fbf6b23facd606bfdcdc42d28c
AoschkA <jonaspraem@gmail.com>
2016-01-14 16:13:11 +0100

    updated help and settings activity

--- app/src/main/java/com/androidudvikling/zeengoone/planpennyv04/ActivityHelp.java
+++ app/src/main/java/com/androidudvikling/zeengoone/planpennyv04/ActivityHelp.java
@@ -1,21 +1,26 @@
 package com.androidudvikling.zeengoone.planpennyv04;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 /**
  * Created by Jonas Praem on 13-Jan-16.
  */
-public class ActivityHelp extends Activity {
+public class ActivityHelp extends Activity implements View.OnClickListener {
 
     ScrollView scrollView;
     ImageView imageView1;
     TextView textView1;
+    TextView finishText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,5 +31,14 @@ public class ActivityHelp extends Activity {
         imageView1 = (ImageView) findViewById(R.id.imageViewHelp);
         textView1 = (TextView) findViewById(R.id.textViewHelp1);
         textView1.setText(R.string.Describtion);
+        finishText = (TextView) findViewById(R.id.textViewHelp2);
+        finishText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == finishText.getId()) {
+            finish();
+        }
     }
 }
--- app/src/main/java/com/androidudvikling/zeengoone/planpennyv04/ActivitySettings.java
+++ app/src/main/java/com/androidudvikling/zeengoone/planpennyv04/ActivitySettings.java
@@ -1,15 +1,19 @@
 package com.androidudvikling.zeengoone.planpennyv04;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Jonas Praem on 11/01/16.
@@ -17,6 +21,8 @@ import android.widget.TextView;
 public class ActivitySettings extends Activity {
 
     ListView list;
+    ArrayList<View> viewList = new ArrayList<View>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,22 @@ public class ActivitySettings extends Activity {
         setContentView(R.layout.activity_settings);
 
         list = (ListView) findViewById(R.id.settingsList);
+        ArrayAdapter<View> adapter = new ArrayAdapter<View>(
+                this,
+                android.R.layout.simple_list_item_1,
+                viewList);
+        list.setAdapter(adapter);
+
+        TextView textView1 = (TextView) findViewById(R.id.textViewProject);
+        TextView textView2 = (TextView) findViewById(R.id.textViewCategory);
+        Switch switchSynchronize = (Switch) findViewById(R.id.synchronize);
+
+        list.addView(textView1, 0);
+        list.addView(textView2, 1);
+        list.addView(switchSynchronize, 2);
+
 
     }
+
+
 }
