d9020bf0b0c9f73cd878d9a396e22d5013b8bcb5
AoschkA <jonaspraem@gmail.com>
2016-01-15 10:57:10 +0100

    settings java class created + xml string file

--- app/src/main/java/com/androidudvikling/zeengoone/planpennyv04/entities/Settings.java
+++ app/src/main/java/com/androidudvikling/zeengoone/planpennyv04/entities/Settings.java
@@ -1,5 +1,7 @@
 package com.androidudvikling.zeengoone.planpennyv04.entities;
 
+import com.androidudvikling.zeengoone.planpennyv04.R;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -7,6 +9,7 @@ import java.util.HashMap;
  * Created by jonasandreassen on 15/01/16.
  */
 public class Settings {
+    private ArrayList<HashMap<String, ArrayList<String>>> settingsList = new ArrayList<HashMap<String, ArrayList<String>>>();
     private HashMap map = new HashMap();
 
     public Settings() {
@@ -14,23 +17,34 @@ public class Settings {
     }
 
     private void populateHashMap() {
-        ArrayList<String> sortTypes = new ArrayList<String>();
+        ArrayList<String> settingTypes = new ArrayList<String>();
         ArrayList<String> synchronizeTypes = new ArrayList<String>();
-        sortTypes.add("Alfabetisk");
-        sortTypes.add("Største først");
-        sortTypes.add("Først planlagte først");
-        sortTypes.add("Først oprettet først");
+        ArrayList<String> sortTypes = new ArrayList<String>();
 
-        synchronizeTypes.add("Google Calendar");
+        settingTypes.add(String.valueOf(R.string.setting1));
+        settingTypes.add(String.valueOf(R.string.setting2));
+        settingTypes.add(String.valueOf(R.string.setting3));
+        sortTypes.add(String.valueOf(R.string.sortsetting1));
+        sortTypes.add(String.valueOf(R.string.sortsetting2));
+        sortTypes.add(String.valueOf(R.string.sortsetting3));
+        sortTypes.add(String.valueOf(R.string.sortsetting4));
+        synchronizeTypes.add(String.valueOf(R.string.syncsetting1));
 
+        HashMap setting1 = new HashMap();
+        setting1.put(settingTypes.get(0), sortTypes);
+        HashMap setting2 = new HashMap();
+        setting2.put(settingTypes.get(1), sortTypes);
+        HashMap setting3 = new HashMap();
+        setting3.put(settingTypes.get(2), synchronizeTypes);
 
-        map.put("1", sortTypes);
-        map.put("2", sortTypes);
-        map.put("3", synchronizeTypes);
+        settingsList.add(setting1);
+        settingsList.add(setting2);
+        settingsList.add(setting3);
+
     }
 
-    public HashMap getHashMap() {
-        return map;
+    public ArrayList<HashMap<String, ArrayList<String>>> getHashMap() {
+        return settingsList;
     }
 
 
