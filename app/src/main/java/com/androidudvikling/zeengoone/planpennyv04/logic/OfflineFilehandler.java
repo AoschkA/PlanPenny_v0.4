b97a84cbc7dec17b71570e3de52bdd182fab39af
alexandervpedersen <Alexander12350@hotmail.com>
2016-01-15 12:55:55 +0100

    Filehandler metoder oprettet

--- app/src/main/java/com/androidudvikling/zeengoone/planpennyv04/logic/OfflineFilehandler.java
+++ app/src/main/java/com/androidudvikling/zeengoone/planpennyv04/logic/OfflineFilehandler.java
@@ -1,7 +1,28 @@
 package com.androidudvikling.zeengoone.planpennyv04.logic;
 
+import com.androidudvikling.zeengoone.planpennyv04.entities.Project;
+
+import java.io.FileOutputStream;
+
 /**
  * Created by alexandervpedersen on 15/01/16.
  */
 public class OfflineFilehandler {
+
+    FileOutputStream fos;
+    String projectHandler = "ProjectHandler";
+
+    OfflineFilehandler(){
+    }
+
+    public Project[] getProjects(){
+
+        return null;
+    }
+
+    public boolean saveProjects(){
+
+        return false;
+    }
+
 }
