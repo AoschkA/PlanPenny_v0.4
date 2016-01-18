5851311e0d673062b9db93e86123313c56e038d0
AoschkA <jonaspraem@gmail.com>
2016-01-18 16:12:24 +0100

    Google API implemented

--- app/src/main/java/com/androidudvikling/zeengoone/planpennyv04/Fragment_Controller.java
+++ app/src/main/java/com/androidudvikling/zeengoone/planpennyv04/Fragment_Controller.java
@@ -1,8 +1,16 @@
 package com.androidudvikling.zeengoone.planpennyv04;
 
+
+import android.accounts.AccountManager;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.design.widget.CoordinatorLayout;
@@ -12,15 +20,20 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.androidudvikling.zeengoone.planpennyv04.entities.Date;
 import com.androidudvikling.zeengoone.planpennyv04.entities.Project;
@@ -33,8 +46,25 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Fragment_Controller extends AppCompatActivity {
@@ -54,6 +84,16 @@ public class Fragment_Controller extends AppCompatActivity {
     private OnlineFilehandler onf;
     private Context ctx;
 
+    GoogleAccountCredential mCredential;
+    private TextView mOutputText;
+    ProgressDialog mProgress;
+
+    static final int REQUEST_ACCOUNT_PICKER = 1000;
+    static final int REQUEST_AUTHORIZATION = 1001;
+    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    private static final String PREF_ACCOUNT_NAME = "accountName";
+    private static final String[] SCOPES = { CalendarScopes.CALENDAR_READONLY };
+
     public static void populateDrawer(){
         projekt_liste.clear();
         for(Project p:dc.getProjects()){
@@ -64,6 +104,26 @@ public class Fragment_Controller extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LinearLayout activityLayout = new LinearLayout(this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        activityLayout.setLayoutParams(lp);
+        activityLayout.setOrientation(LinearLayout.VERTICAL);
+        activityLayout.setPadding(16, 16, 16, 16);
+        // Instantiere google api textview
+        ViewGroup.LayoutParams tlp = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        mOutputText = new TextView(this);
+        mOutputText.setLayoutParams(tlp);
+        mOutputText.setPadding(16, 16, 16, 16);
+        mOutputText.setVerticalScrollBarEnabled(true);
+        mOutputText.setMovementMethod(new ScrollingMovementMethod());
+        activityLayout.addView(mOutputText);
+        setContentView(activityLayout);
+
+
         setContentView(R.layout.main_activity_controller);
 
         ctx = getApplicationContext();
@@ -73,15 +133,20 @@ public class Fragment_Controller extends AppCompatActivity {
         // Gør listen klar og smid den i projekt listen
         populateDrawer();
 
-        // Instantier Firebase
+        // Instantiere Firebase
         Firebase.setAndroidContext(this);
         uRef = new Firebase("https://g26planpenny.firebaseio.com/");
+
+        // Instantiere ProgressDialog
+        mProgress = new ProgressDialog(this);
+        mProgress.setMessage("Calling Google Calendar API ...");
 
         uRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
             }
+
             @Override
             public void onCancelled(FirebaseError firebaseError) {
 
@@ -94,6 +159,12 @@ public class Fragment_Controller extends AppCompatActivity {
         //Opsæt online filehandler
         onf = new OnlineFilehandler(ctx);
 
+        // Instantiere credentials og service objekt
+        SharedPreferences googleSettings = getPreferences(Context.MODE_PRIVATE);
+        mCredential = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff())
+                .setSelectedAccountName(googleSettings.getString(PREF_ACCOUNT_NAME, null));
 
         // Sæt drawer elementer til ProjektVisning
         projekt_liste_view = (ListView) findViewById(R.id.penny_projekt_drawer_list);
@@ -107,7 +178,6 @@ public class Fragment_Controller extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setLogo(R.drawable.penny_logo);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
-
 
         //Opsæt coordinatorlayout:
         coordinatorLayout = (CoordinatorLayout) findViewById(R.id.penny_projekt_drawer_coordinatorlayout);
@@ -127,6 +197,22 @@ public class Fragment_Controller extends AppCompatActivity {
         // Få fat i ViewPager og set dens pageradapter så den kan vise items
 
     }
+
+    // Forklaring fra google: https://developers.google.com/google-apps/calendar/quickstart/android
+    /**
+     * Called whenever this activity is pushed to the foreground, such as after
+     * a call to onCreate().
+     */
+   @Override
+   public void onResume(){
+       super.onResume();
+       if (isGooglePlayServicesAvailable()) {
+           refreshResults();
+       } else {
+           mOutputText.setText("Google Play Services required: " +
+                   "after installing, close and relaunch this app.");
+       }
+   }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -212,10 +298,131 @@ public class Fragment_Controller extends AppCompatActivity {
         penny_Projekt_Drawer_Toggle.syncState();
     }
 
-    @Override
 
-        public void onActivityResult(int requestCode, int resultCode, Intent data) {
-            super.onActivityResult(requestCode, resultCode, data);
+    // Forklaring fra google: https://developers.google.com/google-apps/calendar/quickstart/android
+    /**
+     * Attempt to get a set of data from the Google Calendar API to display. If the
+     * email address isn't known yet, then call chooseAccount() method so the
+     * user can pick an account.
+     */
+
+    private void refreshResults() {
+        if (mCredential.getSelectedAccountName() == null) {
+            chooseAccount();
+        } else {
+            if (isDeviceOnline()) {
+                new MakeRequestTask(mCredential).execute();
+            } else {
+                mOutputText.setText("No network connection available.");
+            }
+        }
+    }
+
+    // Forklaring fra google: https://developers.google.com/google-apps/calendar/quickstart/android
+    /**
+     * Starts an activity in Google Play Services so the user can pick an
+     * account.
+     */
+    private void chooseAccount() {
+        startActivityForResult(
+                mCredential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+    }
+
+    // Forklaring fra google: https://developers.google.com/google-apps/calendar/quickstart/android
+    /**
+     * Checks whether the device currently has a network connection.
+     * @return true if the device has a network connection, false otherwise.
+     */
+    private boolean isDeviceOnline() {
+        ConnectivityManager connMgr =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    // Forklaring fra google: https://developers.google.com/google-apps/calendar/quickstart/android
+    /**
+     * Check that Google Play services APK is installed and up to date. Will
+     * launch an error dialog for the user to update Google Play Services if
+     * possible.
+     * @return true if Google Play Services is available and up to
+     *     date on this device; false otherwise.
+     */
+    private boolean isGooglePlayServicesAvailable() {
+        final int connectionStatusCode =
+                GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (GooglePlayServicesUtil.isUserRecoverableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+            return false;
+        } else if (connectionStatusCode != ConnectionResult.SUCCESS ) {
+            return false;
+        }
+        return true;
+    }
+
+    // Forklaring fra google: https://developers.google.com/google-apps/calendar/quickstart/android
+    /**
+     * Display an error dialog showing that Google Play Services is missing
+     * or out of date.
+     * @param connectionStatusCode code describing the presence (or lack of)
+     *     Google Play Services on this device.
+     */
+    void showGooglePlayServicesAvailabilityErrorDialog(
+            final int connectionStatusCode) {
+        Dialog dialog = GooglePlayServicesUtil.getErrorDialog(
+                connectionStatusCode,
+                Fragment_Controller.this,
+                REQUEST_GOOGLE_PLAY_SERVICES);
+        dialog.show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Kører onActivityResult for google play service
+        // Forklaring fra google: https://developers.google.com/google-apps/calendar/quickstart/android
+        /**
+         * Called when an activity launched here (specifically, AccountPicker
+         * and authorization) exits, giving you the requestCode you started it with,
+         * the resultCode it returned, and any additional data from it.
+         * @param requestCode code indicating which activity result is incoming.
+         * @param resultCode code indicating the result of the incoming
+         *     activity result.
+         * @param data Intent (containing result data) returned by incoming
+         *     activity result.
+         */
+        switch(requestCode) {
+            case REQUEST_GOOGLE_PLAY_SERVICES:
+                if (resultCode != RESULT_OK) {
+                    isGooglePlayServicesAvailable();
+                }
+                break;
+            case REQUEST_ACCOUNT_PICKER:
+                if (resultCode == RESULT_OK && data != null &&
+                        data.getExtras() != null) {
+                    String accountName =
+                            data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        mCredential.setSelectedAccountName(accountName);
+                        SharedPreferences settings =
+                                getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putString(PREF_ACCOUNT_NAME, accountName);
+                        editor.apply();
+                    }
+                } else if (resultCode == RESULT_CANCELED) {
+                    mOutputText.setText("Account unspecified.");
+                }
+                break;
+            case REQUEST_AUTHORIZATION:
+                if (resultCode != RESULT_OK) {
+                    chooseAccount();
+                }
+                break;
+        }
+        // køre onActivityResult igen efter google request til google services
+        super.onActivityResult(requestCode, resultCode, data);
 
                     if (resultCode == 2) {
                         // Projekt
@@ -284,4 +491,106 @@ public class Fragment_Controller extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, vpFragment, "viewpager").commit();
         }
     }
+
+    // Hel klasse taget fra https://developers.google.com/google-apps/calendar/quickstart/android
+    private class MakeRequestTask extends AsyncTask<Void, Void, List<String>> {
+        private com.google.api.services.calendar.Calendar mService = null;
+        private Exception mLastError = null;
+
+        public MakeRequestTask(GoogleAccountCredential credential) {
+            HttpTransport transport = AndroidHttp.newCompatibleTransport();
+            JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+            mService = new com.google.api.services.calendar.Calendar.Builder(
+                    transport, jsonFactory, credential)
+                    .setApplicationName("Google Calendar API Android Quickstart")
+                    .build();
+        }
+
+        // Forklaring fra google: https://developers.google.com/google-apps/calendar/quickstart/android
+        /**
+         * Background task to call Google Calendar API.
+         * @param params no parameters needed for this task.
+         */
+        @Override
+        protected List<String> doInBackground(Void... params) {
+            try {
+                return getDataFromApi();
+            } catch (Exception e) {
+                mLastError = e;
+                cancel(true);
+                return null;
+            }
+        }
+
+        // Forklaring fra google: https://developers.google.com/google-apps/calendar/quickstart/android
+        /**
+         * Fetch a list of the next 10 events from the primary calendar.
+         * @return List of Strings describing returned events.
+         * @throws IOException
+         */
+        private List<String> getDataFromApi() throws IOException {
+            // List the next 10 events from the primary calendar.
+            DateTime now = new DateTime(System.currentTimeMillis());
+            List<String> eventStrings = new ArrayList<String>();
+            Events events = mService.events().list("primary")
+                    .setMaxResults(10)
+                    .setTimeMin(now)
+                    .setOrderBy("startTime")
+                    .setSingleEvents(true)
+                    .execute();
+            List<Event> items = events.getItems();
+
+            for (Event event : items) {
+                DateTime start = event.getStart().getDateTime();
+                if (start == null) {
+                    // All-day events don't have start times, so just use
+                    // the start date.
+                    start = event.getStart().getDate();
+                }
+                eventStrings.add(
+                        String.format("%s (%s)", event.getSummary(), start));
+            }
+            return eventStrings;
+        }
+
+
+        @Override
+        protected void onPreExecute() {
+            mOutputText.setText("");
+            mProgress.show();
+        }
+
+        @Override
+        protected void onPostExecute(List<String> output) {
+            mProgress.hide();
+            if (output == null || output.size() == 0) {
+                mOutputText.setText("No results returned.");
+            } else {
+                output.add(0, "Data retrieved using the Google Calendar API:");
+                mOutputText.setText(TextUtils.join("\n", output));
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mProgress.hide();
+            if (mLastError != null) {
+                if (mLastError instanceof GooglePlayServicesAvailabilityIOException) {
+                    showGooglePlayServicesAvailabilityErrorDialog(
+                            ((GooglePlayServicesAvailabilityIOException) mLastError)
+                                    .getConnectionStatusCode());
+                } else if (mLastError instanceof UserRecoverableAuthIOException) {
+                    startActivityForResult(
+                            ((UserRecoverableAuthIOException) mLastError).getIntent(),
+                            Fragment_Controller.REQUEST_AUTHORIZATION);
+                } else {
+                    mOutputText.setText("The following error occurred:\n"
+                            + mLastError.getMessage());
+                }
+            } else {
+                mOutputText.setText("Request cancelled.");
+            }
+        }
+    }
+
 }
